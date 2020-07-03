1f2f511acca527aea2537a9897e707e9fece1bb3
Marcos Vinícius Emídio Silva <mviniciusess@gmail.com>
2020-07-03 09:09:26 -0300

    Atualizando e excluindo instrutores

--- instructors.js
+++ instructors.js
@@ -1,6 +1,11 @@
 const fs = require('fs')
 const data = require('./data.json')
 const { age, date } = require('./utils')
+const e = require('express')
+
+exports.index = (req, res) => {
+    return res.render('instructors/index', { instructors: data.instructors })
+}
 
 //show
 exports.show = (req, res) => {
@@ -60,7 +65,7 @@ exports.post = (req, res) => {
 
 //edit
 exports.edit = (req, res) => {
-    const { id } = req.params
+    const { id } = req.params  // req.params são parâmetros da url
 
     const foundInstructor = data.instructors.find( instructor => {
         return instructor.id == id
@@ -76,4 +81,48 @@ exports.edit = (req, res) => {
     return res.render('instructors/edit', {instructor: instructor} )
 }
 
+//put
+exports.put = (req, res) => {
+    const { id } = req.body
+    let index = 0
+
+    const foundInstructor = data.instructors.find( (instructor, foundIndex) => {
+        if ( id == instructor.id ) {
+            index = foundIndex
+            return true
+        }
+    })
+
+    if(!foundInstructor) return res.send('Instructor not found!')
+
+    const instructor = {
+        ...foundInstructor,
+        ...req.body,
+        birth: Date.parse(req.body.birth)
+    }
+
+    data.instructors[index] = instructor
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
+        if(err) return res.send('Write error!')
+
+        return res.redirect(`/instructors/${id}`)
+    })
+}
+
 //delete
+exports.delete = (req, res) => {
+    const { id } = req.body
+
+    const filteredInstructors = data.instructors.filter( instructor => {
+        return instructor.id != id
+    })
+
+    data.instructors = filteredInstructors
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), err => {
+        if(err) return res.send('Write error!')
+
+        return res.redirect('/instructors')
+    })
+}
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
 const routes = require('./routes')
+const methodOverride = require('method-override')
 
 const server = express()
 
 server.use(express.urlencoded({ extended: true })) // responsável por dar funcionalidade ao "req.body" em routes.js
 server.use(express.static('public')) //express observa a pasta public para servir ao servidor os arquivos estáticos (arquivos de estilização)
+server.use(methodOverride('_method'))
 server.use(routes)
 
 server.set("view engine", "njk")
